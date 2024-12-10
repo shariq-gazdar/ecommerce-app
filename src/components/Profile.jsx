06567023d3cc320cc89253c58323a8b143b4fb9f
= <gazdarshariq0@gmail.com>
2024-12-10 20:48:12 +0500

    signOut thing add!!!!

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import Home from "../assets/home.svg";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function Profile(props) {
   const user = props.user;
+  const navigate = useNavigate(); // Using useNavigate hook
   const existingUsers = JSON.parse(localStorage.getItem("users")) || [];
   let cartItems = JSON.parse(localStorage.getItem(props.user)) || [];
   let cartNumber = cartItems.length;
@@ -14,6 +15,9 @@ function Profile(props) {
       name = users.name;
     }
   });
+  const signOut = () => {
+    navigate("/login");
+  };
   return (
     <div className="flex justify-center items-stretch gap-y-44 flex-col">
       <span className="ml-[95vw] p-2">
@@ -26,7 +30,10 @@ function Profile(props) {
           <h1 className="text-center font-bold">User :{user}</h1>
           <h1 className="text-center font-bold">Name :{name}</h1>
           <h1 className="text-center font-bold">Items in Cart:{cartNumber}</h1>
-          <button className="text-center w-full bg-red-500 rounded-xl p-2 text-white">
+          <button
+            className="text-center w-full bg-red-500 rounded-xl p-2 text-white"
+            onClick={signOut}
+          >
             SignOut
           </button>
         </span>
